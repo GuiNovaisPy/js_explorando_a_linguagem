18da09ef6b9fd198762621ee9e0cba6f6a61f95c
Guilherme Novais Silva <guilherme.silva@grupoicr.local>
2025-01-31 16:49:15 -0300

    break

--- 8-loops.js
+++ 8-loops.js
@@ -23,16 +23,20 @@ const podeComprar = idadeComprador >= 18 || estaAcompanhada;//retorna um verdade
 
 // };
 let contador = 0;//posicao do item que quero verificar
+let destinoExiste = false;
 while (contador < listaDeDestinos.length) {
     if (listaDeDestinos[contador] == destino){//acessando os itens da lista
-        console.log("o destino esta disponivel na lista")
+        //console.log("o destino esta disponivel na lista");
+        destinoExiste = true
         break;
     }else{
         if(contador == listaDeDestinos.length - 1){// -1 por conta do index partir do zero, e ja o lenght nao considera esse zero ele parte do 1
-            console.log("destino nao encontrado na lista")
-            break
+            //console.log("destino nao encontrado na lista");
+            break;
         }
     };
     contador += 1;//incrementando o contador
 
 };
+
+console.log("destino existe:",destinoExiste)
