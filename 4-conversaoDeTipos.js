76e9d5f08eef9c7678ab2c037123c3c6b4fc6d78
Guilherme Novais Silva <guilherme.silva@grupoicr.local>
2025-01-30 23:48:54 -0300

    Conversao de tipos, str em float, e str em int para calculos

---
+++ 4-conversaoDeTipos.js
@@ -0,0 +1,9 @@
+console.log("Conversão de tipos");
+
+console.log("ano" + 2020);
+console.log("2" + "2") //concatena
+
+console.log(parseInt("2") + parseInt("2")) //converte o tipo para int e soma
+console.log(parseFloat("2.5") + parseFloat("2.5")) //converte para float
+
+
