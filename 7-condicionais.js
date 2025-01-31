facbae3cb8bf74664608e6e419488da84f665aab
Guilherme Novais Silva <guilherme.silva@grupoicr.local>
2025-01-31 15:18:35 -0300

    operadores logicos && == e, and, || == ou, or

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -10,26 +10,55 @@ console.log("destinos possiveis:");
 //console.log(listaDeDestinos);
 
 const idadeComprador = 17;
-const estaAcompanhada = true;//boleando true or false
+const estaAcompanhada = false;//boleando true or false
+const temPassagemComprada = true;
 
 //condicionais
-if (idadeComprador >= 18) {//se a condição for verdadeira
-    console.log("para maiores de idade")
-    console.log(listaDeDestinos)
+//operadores condicionais encadeados
+// if (idadeComprador >= 18 ) {//se a condição for verdadeira
+//     console.log("para maiores de idade")
+//     console.log(listaDeDestinos)
+// } else { // se não
+//     // a pessoa é menor de idade
+
+//     if (estaAcompanhada) {
+//         console.log("para menores de idade acompanhados")
+
+//     } else {
+//         console.log("para menores de idade sem responsavel")
+//         listaDeDestinos.splice(0, 3)//removendo item [0]
+//     }
+
+// };
+// console.log(listaDeDestinos)
+
+
+
+//operadores logicos encadeados
+if (idadeComprador >= 18 || estaAcompanhada) { 
+    console.log("para maiores de idade, ou menores de idade acompanhados")
 } else { // se não
     // a pessoa é menor de idade
-
-    if (estaAcompanhada) {
-        console.log("para menores de idade acompanhados")
-        console.log(listaDeDestinos)
-    } else {
-        console.log("para menores de idade sem responsavel")
-        listaDeDestinos.splice(0, 3)//removendo item [0]
-        console.log(listaDeDestinos)
-    }
-
+    console.log("para menores de idade sem responsavel")
+    listaDeDestinos.splice(0, 3)//removendo item [0]
 
 };
+console.log(listaDeDestinos)
+
+console.log("embarque: \n \n")
+
+// || == ou,or
+// && == and, e
+
+if (idadeComprador >= 18 || estaAcompanhada && temPassagemComprada ){
+    console.log("Boa viagem")
+}else{
+    console.log("sem os requisitos para viajar")
+};
+
+
+
+
 
 
 // operadores logicos
