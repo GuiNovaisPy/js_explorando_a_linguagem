7e436e03eed6e2fe106e5d2865858cb1fa331239
Guilherme Novais Silva <guilherme.silva@grupoicr.local>
2025-01-31 14:57:38 -0300

    ifs cadeados

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -9,22 +9,32 @@ const listaDeDestinos = new Array(
 console.log("destinos possiveis:");
 //console.log(listaDeDestinos);
 
-const idadeComprador = 18;
+const idadeComprador = 17;
+const estaAcompanhada = true;//boleando true or false
 
 //condicionais
-if(idadeComprador >= 18){//se a condição for verdadeira
+if (idadeComprador >= 18) {//se a condição for verdadeira
     console.log("para maiores de idade")
     console.log(listaDeDestinos)
-}else{ // se não
-    console.log("para menores de idade")
-    listaDeDestinos.splice(0,3)//removendo item [0]
-    console.log(listaDeDestinos)
+} else { // se não
+    // a pessoa é menor de idade
+
+    if (estaAcompanhada) {
+        console.log("para menores de idade acompanhados")
+        console.log(listaDeDestinos)
+    } else {
+        console.log("para menores de idade sem responsavel")
+        listaDeDestinos.splice(0, 3)//removendo item [0]
+        console.log(listaDeDestinos)
+    }
+
+
 };
 
 
 // operadores logicos
-// console.log(idadeComprador > 18);//maior que 
+// console.log(idadeComprador > 18);//maior que
 // console.log(idadeComprador < 18);//menor que
-// console.log(idadeComprador >= 18);// maior ou igual a 
-// console.log(idadeComprador <= 18);// menor ou igual a 
+// console.log(idadeComprador >= 18);// maior ou igual a
+// console.log(idadeComprador <= 18);// menor ou igual a
 // console.log(idadeComprador == 18);// igual a
