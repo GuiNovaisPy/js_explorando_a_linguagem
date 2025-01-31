a55fdfa39861f29ae86c9dd91ee96cd68a9932c7
Guilherme Novais Silva <guilherme.silva@grupoicr.local>
2025-01-31 13:08:05 -0300

    removendo itens da lista com splice, acessando um item especifico da lista, contando quantos itens tem nessa lista

--- 6-listas.js
+++ 6-listas.js
@@ -15,3 +15,14 @@ listaDeDestinos.push(`Curitiba`) //adicionando mais itens a lista
 
 console.log("Destinos possiveis:");
 console.log(listaDeDestinos) //exibe a lista inteira, inclusive o objeto
+
+
+listaDeDestinos.splice(1,1)// o primeiro parametro é o start ou seja nesse caso ele vai apagar o item de posicao 1 , o segundo parametro é a quantidade de itens que voce quer remover deste ponto em diante
+console.log(listaDeDestinos)
+
+
+//acessando a lista
+console.log(listaDeDestinos[1]) //exibindo somente o index 01 dessa lista
+
+//contando itens da lista
+console.log(listaDeDestinos.length) //exibe a quantidade de itens na lista
