ab0a37d25ddcb04094f3028518d72d38d27f8b3c
Guilherme Novais Silva <guilherme.silva@grupoicr.local>
2025-01-31 16:30:12 -0300

    Trabalhando com loops (while) contadores e ifs dentro do loop

---
+++ 8-loops.js
@@ -0,0 +1,38 @@
+console.log("Trabalhando com loops");
+
+const listaDeDestinos = new Array(
+    'SÃO PAULO',
+    'RIO DE JANEIRO',
+    'CURITIBA'
+);
+
+//console.log(listaDeDestinos);
+
+const idadeComprador = 18;
+const estaAcompanhada = false;//boleando true or false
+let temPassagemComprada = false;//variavel
+const destino = 'SÃO PAULO';
+
+const podeComprar = idadeComprador >= 18 || estaAcompanhada;//retorna um verdadeiro ou falso para a constante podeComprar
+
+
+// let contador = 0;//posicao do item que quero verificar
+// while (contador < listaDeDestinos.length) {
+//     console.log(listaDeDestinos[contador]);//acessando os itens da lista
+//     contador += 1;//incrementando o contador
+
+// };
+let contador = 0;//posicao do item que quero verificar
+while (contador < listaDeDestinos.length) {
+    if (listaDeDestinos[contador] == destino){//acessando os itens da lista
+        console.log("o destino esta disponivel na lista")
+        break;
+    }else{
+        if(contador == listaDeDestinos.length - 1){// -1 por conta do index partir do zero, e ja o lenght nao considera esse zero ele parte do 1
+            console.log("destino nao encontrado na lista")
+            break
+        }
+    };
+    contador += 1;//incrementando o contador
+
+};
